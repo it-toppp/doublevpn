4108f5cb06f3c24dee93163022cfb2a1e07dc40a
it-toppp <it.toppp@gmail.com>
2018-11-03 11:08:15 +0300

    beta

--- install.sh
+++ install.sh
@@ -2,32 +2,13 @@
 
 CNF=$(cat  /root/doublevpn/wg-client.conf);
 
-###################
-# Install git #
-apt-get install git -y
-###################
-# Install ansible #
-if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    echo "Adding Ansible PPA"
-    apt-add-repository ppa:ansible/ansible -y
-fi
-
-if ! hash ansible >/dev/null 2>&1; then
-    echo "Installing Ansible..."
-    apt-get update
-    apt-get install software-properties-common ansible git python-apt -y
-else
-    echo "Ansible already installed"
-fi
-
-git clone https://github.com/it-toppp/doublevpn.git && cd /root/doublevpn/
 #####################################
 # Display real installation process #
 
 cd ./doublevpn
 ansible-playbook gen_conf.yml
 ansible-playbook main.yml
-
+echo  "Please copy this content to your TunSafe configuration file"
 echo "######################################################################################################################################"
 echo ""
 echo " $CNF"
